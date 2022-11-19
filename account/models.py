8a303f2b525c475378139e0c2c64034654a1ae68
teerenzo <renzahoemmanuel8@gmail.com>
2022-11-19 09:45:09 +0200

    create models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Hospital(models.Model):
+      name=models.CharField(max_length=200)
+      address=models.CharField(max_length=200)
+      user=models.ForeignKey(User,on_delete=models.CASCADE)
+      created_date=models.DateTimeField(auto_now=True)
+
+      class Meta:
+        ordering=['-created_date']
+
+      def __str__(self):
+            return self.name
+
+class Service(models.Model):
+    name=models.CharField(max_length=255)
+    hospital=models.ForeignKey(Hospital,on_delete=models.CASCADE)
+    created_date=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Doctor(models.Model):
+    names=models.CharField(max_length=225)
+    hospital=models.ForeignKey(Hospital,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    service=models.ForeignKey(Service,on_delete=models.CASCADE)
+    created_date=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.names
+
+class Patient(models.Model):
+    names=models.CharField(max_length=255)
+    address=models.CharField(max_length=255)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    created_date=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.names
+
+
+class RequestAppointment(models.Model):
+    STATUS = (("approved", "Approved"), ("pending", "Pending"),("canceled", "Canceled"), ("rejected", "Rejected"))
+    patient=models.ForeignKey(Patient,on_delete=models.CASCADE)
+    service=models.ForeignKey(Service,on_delete=models.CASCADE)
+    status = models.CharField(max_length=123, choices=STATUS, default="pending")
+    created_date=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.patient.names
+
+
